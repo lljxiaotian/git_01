ae06b7a56beffcd7e9d79d1679ea793ce72928be
Administrator <730711385@qq.com>
2021-04-05 14:39:18 +0800

    第一次提交

---
+++ TestProject/src/Test_1.java
@@ -0,0 +1,11 @@
+public class Test_1 {
+
+    public static void main(String[] args) {
+
+        int a=1;
+        String name="luolianjie";
+
+        System.out.println(name+"age is "+a);
+    }
+
+}
